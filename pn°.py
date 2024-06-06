4bbc39ca10e802e94c56898e95ebeee7897f0f22
guilherme444i <166552605+guilherme444i@users.noreply.github.com>
2024-06-06 16:53:15 -0300

    Add files via upload

---
+++ pn°.py
@@ -0,0 +1,9 @@
+soma = 0
+
+while True:
+    numero = int(input("Digite um número inteiro (digite 0 para parar): "))
+    if numero == 0:
+        break
+    soma += numero
+
+print("A soma de todos os números digitados é:", soma)
