e15f9acb547650949cfaf0db6dae88aaf7607642
guilherme444i <166552605+guilherme444i@users.noreply.github.com>
2024-06-11 16:57:38 -0300

    Add files via upload

---
+++ Alunos#2.py.completo.py
@@ -0,0 +1,8 @@
+Lista= []
+
+Lista_nomes=["Jucelina" , "Rhenan" , "Gabriel Nobre" , "Cristian Roveré" , "Guilherme" , "Gabriel Pereira" , "Daniel Holanda" , "Marcelo" , "Felipe" , "Maria julia" , "Luan" , "Leticia" , "Ana Luiza" , "Maria" , "Nicole" , "Kuan" , "Geovani"]
+
+print('nomes:')
+for nomes in Lista:
+        print(Lista)
+
