3364a409c5d8e127cc4d962bd9eb19fff5420cdc
guilherme444i <166552605+guilherme444i@users.noreply.github.com>
2024-06-11 16:57:57 -0300

    Delete Alunos#2.py.completo.py

--- Alunos#2.py.completo.py
+++
@@ -1,8 +0,0 @@
-Lista= []
-
-Lista_nomes=["Jucelina" , "Rhenan" , "Gabriel Nobre" , "Cristian Roveré" , "Guilherme" , "Gabriel Pereira" , "Daniel Holanda" , "Marcelo" , "Felipe" , "Maria julia" , "Luan" , "Leticia" , "Ana Luiza" , "Maria" , "Nicole" , "Kuan" , "Geovani"]
-
-print('nomes:')
-for nomes in Lista:
-        print(Lista)
-
